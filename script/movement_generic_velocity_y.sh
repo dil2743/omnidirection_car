e93011da6a859ead51dad1ad12d23cb2f27e627c
dilshad khan <dilshadk@dosepack.com>
2021-07-10 12:57:25 +0530

    adding files

---
+++ script/movement_generic_velocity_y.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rostopic pub -f movement_generic_velocity_y.bagy /omnidirection_car/command omnidirection_car/Movement
---
+++ script/movement_stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rostopic pub -f movement_stop.bagy /omnidirection_car/command omnidirection_car/Movement
